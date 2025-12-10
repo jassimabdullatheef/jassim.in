dee48c8ee988917f413d128ebf92e004a8b48923
Work <work@MacBook-Pro.local>
2025-12-10 20:08:53 +0300

    added timeout var

--- src/routes/projects/timeblocker/api/process-tasks/+server.js
+++ src/routes/projects/timeblocker/api/process-tasks/+server.js
@@ -1,6 +1,9 @@
 import { json } from "@sveltejs/kit";
 import { processTasks } from "../../agent.js";
 
+// For vercel edge functions, we need to set a max duration
+export const maxDuration = 30;
+
 /**
  * POST endpoint to process a text block and extract structured task data
  *
